5e14ce8df1f6a6bf1f505d969d819066d2d9c7d1
TreyG <TreyBGreenMN24@gmail.com>
2018-09-25 23:51:20 -0500

    Calculates geocentric orbit path
    Uses orbital elements to calculate the coordinates of the geocentric orbit path.

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 clear,clc
 
-r0 = [-0.707;0.707;0];
-v0 = [0;0.5;0];
+r0 = [5;0.707;.5];
+v0 = [-0.5;0.1;-0.1];
 mu = 1;
 
 [p,a,e,i,omega,Omega,nu0] = classical_orbital_elements(r0,v0,mu);
@@ -14,18 +14,7 @@ fprintf("Arguement of Periapsis:\t%f\n",omega);
 fprintf("Ascending Node:\t\t\t%f\n",Omega);
 fprintf("True Anomoly Angle:\t\t%f\n",nu0);
 
-A = [1,0,0;0,cosd(180),sind(180);0,-sind(180),cosd(180)];
-
-nu = (0:0.001:2*pi)';
-r = p./(1+e*cos(nu));
-phi = zeros(length(nu),1);
-x = zeros(length(nu),1);
-y = zeros(length(nu),1);
-z = zeros(length(nu),1);
-
-for i=1:length(nu)
-    [x(i),y(i),z(i)] = sph2cart(nu(i),phi(i),r(i));
-end
+[x,y,z] = orbital_elem2geo_eq(p,e,i,omega,Omega);
 
 figure(1)
 plot_orbit(x,y,z);
---
+++ orbital_elem2geo_eq.m
@@ -0,0 +1,36 @@
+function [x,y,z] = orbital_elem2geo_eq(p,e,i,omega,Omega)
+%ORBITAL_ELEM2GEO_EQ Transforms Perifocal nu,r coordinates to 3D Geocentric
+%Equatorial coordinates
+%   Uses semi-latus rectum (p), eccentricity (e), inclination angle (i), 
+%   argument of periapsis (omega), and the ascending node (Omega) to
+%   transform the coordinates between systems. Uses the transform_matrix
+%   function to return matrices.
+
+nu = (0:0.001:2*pi)';
+r = p./(1+e*cos(nu));
+phi = zeros(length(nu),1);
+
+[xp,yp,zp] = sph2cart(nu,phi,r); 
+transform = [1,0,0;0,1,0;0,0,1];
+if ~isnan(omega)
+    transform = transform * transform_matrix(omega,3);
+end
+if ~isnan(i)
+    transform = transform * transform_matrix(i,1);
+end
+if ~isnan(Omega)
+    transform = transform * transform_matrix(Omega,3);
+end
+
+x = zeros(length(xp),1);
+y = zeros(length(xp),1);
+z = zeros(length(xp),1);
+for len = 1:length(xp)
+    trans = transform' * [xp(len);yp(len);zp(len)];
+    x(len) = trans(1);
+    y(len) = trans(2);
+    z(len) = trans(3);
+end
+return
+end
+
