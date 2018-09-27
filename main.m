81c7a179b28a9cf015e24e953d8e33c044ff1404
deberl <deberl@iastat.edu>
2018-09-26 20:24:37 -0500

    Formatting output

--- main.m
+++ main.m
@@ -7,12 +7,12 @@ mu = 1;
 [p,a,e,i,omega,Omega,nu0] = classical_orbital_elements(r0,v0,mu);
 
 fprintf('Semi-Latus Rectum (p):\t\t%f\n',p);
-fprintf('Semi-Major Axis:\t\t%f\n',a);
-fprintf('Eccentricity:\t\t\t%f\n',e);
-fprintf('Inclination Angle:\t\t%f\n',i);
-fprintf('Arguement of Periapsis:\t\t%f\n',omega);
-fprintf('Ascending Node:\t\t\t%f\n',Omega);
-fprintf('True Anomoly Angle:\t\t%f\n',nu0);
+fprintf('Semi-Major Axis (a):\t\t%f\n',a);
+fprintf('Eccentricity (e):\t\t%f\n',e);
+fprintf('Inclination Angle (i):\t\t%f\n',i);
+fprintf('Argument of Periapsis (Omega):\t%f\n',omega);
+fprintf('Ascending Node (omega):\t\t%f\n',Omega);
+fprintf('True Anomoly Angle (nu_0):\t%f\n',nu0);
 
 [x,y,z] = orbital_elem2geo_eq(p,e,i,omega,Omega);
 
